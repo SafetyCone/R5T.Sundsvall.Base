3076bde89904bb016e621402d5fd02508cccab64
MinexAutomation <minex.automation@gmail.com>
2019-11-29 23:06:11 -0800

    Added endpoint-type repository service definition.

---
+++ source/R5T.Sundsvall.Base/Code/Services/Interfaces/IEndpointTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Endalia;
+
+
+namespace R5T.Sundsvall
+{
+    public interface IEndpointTypeRepository
+    {
+        EndpointTypeIdentity New();
+
+        void Add(EndpointTypeInfo endpointType);
+
+        bool Exists(EndpointTypeIdentity identity);
+
+        EndpointTypeInfo GetInfo(EndpointTypeIdentity identity);
+        IEnumerable<EndpointTypeInfo> GetAllInfos();
+
+        void SetName(EndpointTypeIdentity identity, string name);
+        string GetName(EndpointTypeIdentity identity);
+
+        void Delete(EndpointTypeIdentity identity);
+    }
+}
