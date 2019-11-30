7d98cfa14ebfc1df2bd13e0905bb96200c255832
MinexAutomation <minex.automation@gmail.com>
2019-11-30 00:15:22 -0800

    Added an extension method to simplify adding endpoint type infos.

---
+++ source/R5T.Sundsvall.Base/Code/Services/Extensions/EndpointTypeRepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using R5T.Endalia;
+
+
+namespace R5T.Sundsvall
+{
+    public static class EndpointTypeRepositoryExtensions
+    {
+        public static void Add(this IEndpointTypeRepository repository, string name)
+        {
+            var endpointTypeInfo = new EndpointTypeInfo()
+            {
+                Identity = EndpointTypeIdentity.GetNewGuid(),
+                Name = name,
+            };
+
+            repository.Add(endpointTypeInfo);
+        }
+    }
+}
