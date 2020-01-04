46361cb4f91e7b0240b038cf9e207b3885e83937
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:25:24 -0800

    Added methods. Added asynchronous methods.

--- source/R5T.Sundsvall.Base/Code/Services/Interfaces/IEndpointRepository.cs
+++ source/R5T.Sundsvall.Base/Code/Services/Interfaces/IEndpointRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using R5T.Corcyra;
 using R5T.Endalia;
@@ -16,8 +17,13 @@ namespace R5T.Sundsvall
         EndpointInfo GetInfo(EndpointIdentity identity);
         IEnumerable<EndpointInfo> GetAllInfos();
 
+        Task SetNameAsync(EndpointIdentity endpoint, string name);
+
         void SetEndpointType(EndpointIdentity endpoint, EndpointTypeIdentity endpointType);
+        Task SetEndpointTypeAsync(EndpointIdentity endpoint, EndpointTypeIdentity endpointType);
         EndpointTypeIdentity GetEndpointType(EndpointIdentity endpoint);
+
+        Task<bool> CatchmentHasEndpoint(CatchmentIdentity catchmentIdentity);
 
         void SetEndpointForCatchment(CatchmentIdentity catchment, EndpointIdentity endpoint);
         EndpointIdentity GetEndpointForCatchment(CatchmentIdentity catchment);
