8db63aeeb36dd9c7c541fa14705c2e96903fff65
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:25:51 -0800

    Added extensions for the IEndpointRepository.

---
+++ source/R5T.Sundsvall.Base/Code/Services/Extensions/IEndpointRepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using R5T.Corcyra;
+using R5T.Endalia;
+
+
+namespace R5T.Sundsvall
+{
+    public static class IEndpointRepositoryExtensions
+    {
+        public static EndpointInfo GetInfo(this IEndpointRepository repository, Guid identity)
+        {
+            var endpointInfo = repository.GetInfo(new EndpointIdentity(identity));
+            return endpointInfo;
+        }
+
+        public static EndpointInfo GetInfoForCatchment(this IEndpointRepository repository, CatchmentIdentity catchmentIdentity)
+        {
+            var endpointIdentityForCatchment = repository.GetEndpointForCatchment(catchmentIdentity);
+
+            var endpointInfo = repository.GetInfo(endpointIdentityForCatchment);
+            return endpointInfo;
+        }
+    }
+}
