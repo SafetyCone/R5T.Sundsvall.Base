9575cd8e34f1cd643c676c0263d3b53183278e79
MinexAutomation <minex.automation@gmail.com>
2020-07-19 10:52:59 -0700

    Added IEndpointAndTypeRepository service definition. Plural methods in definition, singular methods as extensions.

---
+++ source/R5T.Sundsvall.Base/Code/Services/Extensions/IEndpointAndTypeRepositoryExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using R5T.Endalia;
+using R5T.Magyar;
+
+
+namespace R5T.Sundsvall
+{
+    public static class IEndpointAndTypeRepositoryExtensions
+    {
+        public static async Task<string> GetEndpointTypeName(this IEndpointAndTypeRepository endpointAndTypeRepository, EndpointIdentity endpointIdentity)
+        {
+            // Purposefully made singular case from plural, even though extra work on creating Dictionary and List objects.
+
+            var endpointIdentities = EnumerableHelper.From(endpointIdentity);
+
+            var endpointIdentitiesByEndpointTypeName = await endpointAndTypeRepository.GetEndpointIdentitiesByEndpointTypeName(endpointIdentities);
+
+            var endpointTypeName = endpointIdentitiesByEndpointTypeName.Keys.Single();
+            return endpointTypeName;
+        }
+    }
+}
---
+++ source/R5T.Sundsvall.Base/Code/Services/Interfaces/IEndpointAndTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Endalia;
+
+
+namespace R5T.Sundsvall
+{
+    public interface IEndpointAndTypeRepository
+    {
+        Task<Dictionary<string, List<EndpointIdentity>>> GetEndpointIdentitiesByEndpointTypeName(IEnumerable<EndpointIdentity> endpointIdentities);
+    }
+}
