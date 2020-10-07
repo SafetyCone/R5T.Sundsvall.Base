451f96dfcd99e81e6f480ae24a68d0be83ba198a
Tasha Arvanitis <tasha.arvanitis@gmail.com>
2020-10-07 14:48:53 -0500

    Add HasEndpointTypeName method
    Helps with malformed database entries like if an organization thinks it's connected to an endpoint
    but the endpoint doesn't exist

--- source/R5T.Sundsvall.Base/Code/Services/Extensions/IEndpointAndTypeRepositoryExtensions.cs
+++ source/R5T.Sundsvall.Base/Code/Services/Extensions/IEndpointAndTypeRepositoryExtensions.cs
@@ -10,6 +10,22 @@ namespace R5T.Sundsvall
 {
     public static class IEndpointAndTypeRepositoryExtensions
     {
+        public static async Task<(bool, string)> HasEndpointTypeName(this IEndpointAndTypeRepository endpointAndTypeRepository, EndpointIdentity endpointIdentity)
+        {
+            var endpointIdentities = EnumerableHelper.From(endpointIdentity);
+
+            var endpointIdentitiesByEndpointTypeName = await endpointAndTypeRepository.GetEndpointIdentitiesByEndpointTypeName(endpointIdentities);
+
+            var hasEndpointTypeName = endpointIdentitiesByEndpointTypeName.Keys.Count() > 0;
+            if (hasEndpointTypeName)
+            {
+                var endpointTypeName = endpointIdentitiesByEndpointTypeName.Keys.Single();
+                return (hasEndpointTypeName, endpointTypeName);
+            }
+
+            return (hasEndpointTypeName, null);
+        }
+
         public static async Task<string> GetEndpointTypeName(this IEndpointAndTypeRepository endpointAndTypeRepository, EndpointIdentity endpointIdentity)
         {
             // Purposefully made singular case from plural, even though extra work on creating Dictionary and List objects.
