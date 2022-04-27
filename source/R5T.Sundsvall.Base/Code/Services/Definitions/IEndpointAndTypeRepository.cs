ccd1e7a65274b084aaceb56dd810e2f70040e3d2
davidcoats <david@getsafetycone.com>
2022-04-27 15:36:01 -0700

    Interim changes.

---
+++ source/R5T.Sundsvall.Base/Code/Services/Definitions/IEndpointAndTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Endalia;
+
+using R5T.T0064;
+
+
+namespace R5T.Sundsvall
+{
+    [ServiceDefinitionMarker]
+    public interface IEndpointAndTypeRepository : IServiceDefinition
+    {
+        Task<Dictionary<string, List<EndpointIdentity>>> GetEndpointIdentitiesByEndpointTypeName(IEnumerable<EndpointIdentity> endpointIdentities);
+    }
+}
---
+++ source/R5T.Sundsvall.Base/Code/Services/Definitions/IEndpointTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using R5T.Endalia;
+
+using R5T.T0064;
+
+
+namespace R5T.Sundsvall
+{
+    [ServiceDefinitionMarker]
+    public interface IEndpointTypeRepository : IServiceDefinition
+    {
+        Task<EndpointTypeIdentity> New();
+
+        Task Add(EndpointTypeInfo endpointType);
+
+        Task<bool> Exists(EndpointTypeIdentity identity);
+
+        Task<EndpointTypeInfo> GetInfo(EndpointTypeIdentity identity);
+        Task<IEnumerable<EndpointTypeInfo>> GetAllInfos();
+
+        Task SetName(EndpointTypeIdentity identity, string name);
+        Task<string> GetName(EndpointTypeIdentity identity);
+
+        Task Delete(EndpointTypeIdentity identity);
+    }
+}
